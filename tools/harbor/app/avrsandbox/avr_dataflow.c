b07079e49a4e76dd0f84bc6294ed83eae49fe117
ram <ram@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-01-23 20:23:48 +0000

    Data flow analysis

--- tools/harbor/app/avrsandbox/avr_dataflow.c
+++ tools/harbor/app/avrsandbox/avr_dataflow.c
@@ -12,19 +12,47 @@
 #include <basicblock.h>
 #include <avrsandbox.h>
 
+//-------------------------------------------------------------------
+// TYPEDEFS
 typedef struct _str_ptr_reg {
   int binit;
+  int reg;
   int lvalue;
   int uvalue;
   int cvalue;
 } ptr_reg_t;
 
+//-------------------------------------------------------------------
+// STATIC FUNCTIONS
+static inline void init_ptr_reg(ptr_reg_t* ptr, int reg);
+static void update_ptr_reg(ptr_reg_t* ptr, avr_instr_t* instr);
+//-------------------------------------------------------------------
 void avr_dataflow_basic_block(basicblk_t* cblk)
 {
+  int i;
   ptr_reg_t x, y, z;
-  x.binit = 0; x.lvalue = 0; x.uvalue = 0; x.cvalue = 0;
-  y.binit = 0; y.lvalue = 0; y.uvalue = 0; y.cvalue = 0;
-  z.binit = 0; z.lvalue = 0; z.uvalue = 0; z.cvalue = 0;
+  init_ptr_reg(&x, R26);
+  init_ptr_reg(&y, R28);
+  init_ptr_reg(&z, R30);
+  
+  for (i = 0; i < (int)(cblk->size/sizeof(avr_instr_t)); i++){
+    update_ptr_reg(&x, &cblk->instr[i]);
+    update_ptr_reg(&y, &cblk->instr[i]);
+    update_ptr_reg(&z, &cblk->instr[i]);
+  }
+
+
   return;
 }
 //-------------------------------------------------------------------
+static void init_ptr_reg(ptr_reg_t* ptr, int reg)
+{
+  ptr->binit = 0; ptr->lvalue = 0; ptr->uvalue = 0; ptr->cvalue = 0;
+  ptr->reg = reg;
+  return;
+}
+//-------------------------------------------------------------------
+static void update_ptr_reg(ptr_reg_t* ptr, avr_instr_t* instr)
+{
+  return;
+}
