a651a61c5525a8726104d8e07db71644b59047ab
ram <ram@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-01-25 01:12:54 +0000

    Pruned platforms directory

--- platform/xyz/include/hardware_types.h
+++
@@ -1,9 +0,0 @@
-/* -*- Mode: C; tab-width:4 -*- */
-/* ex: set ts=4: */
-
-#ifndef _HARDWARE_TYPES_H_
-#define _HARDWARE_TYPES_H_
-#include <hardware_proc.h>
-
-#endif
-
--- platform/xyz/include/photo.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PHOTO_H_
-#define _PHOTO_H_
-
-int8_t photosensor_init();
-
-#endif //_PHOTO_H_
