14d62dbef1d1d5eb8c7fc042b5e4bff7905d701b
schmid <schmid@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-02-01 06:23:57 +0000

    modified the sensorboard to use ADC1 instead of 4 for the X-axis.

--- modules/sensordrivers/h34c/include/h34c.h
+++ modules/sensordrivers/h34c/include/h34c.h
@@ -21,13 +21,8 @@ enum {
 #define H34C_ACCEL_1_TYPE ACCELEROMETER_SENSOR
 #define H34C_ACCEL_2_TYPE ACCELEROMETER_SENSOR
 
-// add port mapping for mts3XX
-// Mica2 uses ADC1 for the CC1000
-#ifdef MICA2_PLATFORM
-#define H34C_ACCEL_0_HW_CH ADC_PROC_CH4
-#else
+// add port mapping for h34c
 #define H34C_ACCEL_0_HW_CH ADC_PROC_CH1
-#endif
 #define H34C_ACCEL_1_HW_CH ADC_PROC_CH2
 #define H34C_ACCEL_2_HW_CH ADC_PROC_CH3
 
