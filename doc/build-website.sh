f66d6c7c68d6150ee57b7dffc918449b672cda32
schmid <schmid@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-02-03 23:22:02 +0000

    added icon support

--- doc/build-website.sh
+++ doc/build-website.sh
@@ -8,21 +8,21 @@ LAYOUT=layout          # Tables based layout.
 
 ASCIIDOC_HTML="asciidoc --unsafe --backend=xhtml11 --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
 
-$ASCIIDOC_HTML --conf-file=${LAYOUT}.conf --attribute=index-only index.txt
+$ASCIIDOC_HTML --conf-file=${LAYOUT}.conf --attribute iconsdir=./icons --attribute=index-only index.txt
 
 cd publications
 $ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. publications.txt
 cd ..
 
 cd tutorial/timesync
-$ASCIIDOC_HTML --conf-file=../../${LAYOUT}.conf --attribute=styledir=../.. timesync.txt
+$ASCIIDOC_HTML --conf-file=../../${LAYOUT}.conf --attribute=styledir=../.. --attribute iconsdir=../../icons timesync.txt
 cd ../..
 
 cd tutorial
-$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. index.txt
-$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. blink.txt
-$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. blink_avrora.txt
-$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. blink_sim.txt
-$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. module_prog_guide.txt
+$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute iconsdir=../icons --attribute=styledir=.. index.txt
+$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute iconsdir=../icons --attribute=styledir=.. blink.txt
+$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute iconsdir=../icons --attribute=styledir=.. blink_avrora.txt
+$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute iconsdir=../icons --attribute=styledir=.. blink_sim.txt
+$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute iconsdir=../icons --attribute=styledir=.. module_prog_guide.txt
 cd ..
 
