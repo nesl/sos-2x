ecce2d3d443d6cbaf9d03239e820008964d44c08
schmid <schmid@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-02-03 00:36:33 +0000

    Created documentation directory and started index.txt for asciidoc. Copied layout from default asciidoc example website.

---
+++ doc/build-website.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VERS="2.0.0"
+DATE="2007-02-03"
+
+# Leave the desired layout uncommented.
+LAYOUT=layout1          # Tables based layout.
+#LAYOUT=layout2         # CSS based simulated frames layout.
+
+ASCIIDOC_HTML="asciidoc --unsafe --backend=xhtml11 --conf-file=${LAYOUT}.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
+
+$ASCIIDOC_HTML --attribute=index-only index.txt
+#$ASCIIDOC_HTML --attribute=numbered userguide.txt
+#$ASCIIDOC_HTML --doctype=manpage manpage.txt
+#$ASCIIDOC_HTML downloads.txt
+#$ASCIIDOC_HTML README.txt
+#$ASCIIDOC_HTML CHANGELOG.txt
+#$ASCIIDOC_HTML README-website.txt
+#$ASCIIDOC_HTML a2x.1.txt
+
