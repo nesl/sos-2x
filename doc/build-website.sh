91d305ba977d9e71f154e118e61520e9e785439c
schmid <schmid@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-02-03 19:13:59 +0000

    updated build scripts to understand hierarchical directories

--- doc/build-website.sh
+++ doc/build-website.sh
@@ -6,14 +6,10 @@ DATE="2007-02-03"
 # Leave the desired layout uncommented.
 LAYOUT=layout          # Tables based layout.
 
-ASCIIDOC_HTML="asciidoc --unsafe --backend=xhtml11 --conf-file=${LAYOUT}.conf --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
+ASCIIDOC_HTML="asciidoc --unsafe --backend=xhtml11 --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
 
-$ASCIIDOC_HTML --attribute=index-only index.txt
-#$ASCIIDOC_HTML --attribute=numbered userguide.txt
-#$ASCIIDOC_HTML --doctype=manpage manpage.txt
-#$ASCIIDOC_HTML downloads.txt
-#$ASCIIDOC_HTML README.txt
-#$ASCIIDOC_HTML CHANGELOG.txt
-#$ASCIIDOC_HTML README-website.txt
-#$ASCIIDOC_HTML a2x.1.txt
+$ASCIIDOC_HTML --conf-file=${LAYOUT}.conf --attribute=index-only index.txt
 
+cd publications
+$ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. publications.txt
+cd ..
