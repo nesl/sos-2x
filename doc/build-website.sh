4dc06782c69194f7f569713bbb53d59e0b6f5ec7
rshea <rshea@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-02-04 02:18:01 +0000

    Basic ack page thanking those who have helped with SOS.

--- doc/build-website.sh
+++ doc/build-website.sh
@@ -9,6 +9,7 @@ LAYOUT=layout          # Tables based layout.
 ASCIIDOC_HTML="asciidoc --unsafe --backend=xhtml11 --attribute icons --attribute iconsdir=./images/icons --attribute=badges --attribute=revision=$VERS  --attribute=date=$DATE"
 
 $ASCIIDOC_HTML --conf-file=${LAYOUT}.conf --attribute iconsdir=./icons --attribute=index-only index.txt
+$ASCIIDOC_HTML --conf-file=${LAYOUT}.conf --attribute iconsdir=./icons --attribute=index-only acknowledgments.txt
 
 cd publications
 $ASCIIDOC_HTML --conf-file=../${LAYOUT}.conf --attribute=styledir=.. publications.txt
