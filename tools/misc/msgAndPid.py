1a286ce057c958bc2c2332fa32b8b871b184cbdd
rshea <rshea@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-04-05 22:55:11 +0000

    Tool for parsing magic numbers out of an elf file.

---
+++ tools/misc/msgAndPid.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+if __name__ == '__main__':
+
+    file = sys.argv[1]
+    msg = os.popen("objdump -G " + file + " | grep MSG")
+    pid = os.popen("objdump -G " + file + " | grep PID")
+
+    msgMapping = {}
+    pidMapping = {}
+
+    for line in msg:
+        pairs = line.split()[6].split(';')[1][1:].split(',')
+        for pair in pairs:
+            if "MSG" in pair:
+                str, num = pair.split(':')
+                msgMapping[str] = num
+
+    for line in pid:
+        pairs = line.split()[6].split(';')[1][1:].split(',')
+        for pair in pairs:
+            if "PID" in pair:
+                str, num = pair.split(':')
+                pidMapping[str] = num
+
+
+    print "\n### MSG IDs ###"
+    for pair in [(k,msgMapping[k]) for k in sorted(msgMapping.keys())]:
+        print "%3s (0x%2x)\t%s"%(pair[1], int(pair[1]), pair[0])
+
+    print "\n### MSG IDs ###"
+    for pair in [(k,pidMapping[k]) for k in sorted(pidMapping.keys())]:
+        print "%3s (0x%2x)\t%s"%(pair[1], int(pair[1]), pair[0])
