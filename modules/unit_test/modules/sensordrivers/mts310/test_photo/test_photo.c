957a15b4e7b0c791149af94e5f988d9c8f0e74ec
simonhan <simonhan@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-06-02 04:56:55 +0000

    add "const"

--- modules/unit_test/modules/sensordrivers/mts310/test_photo/test_photo.c
+++ modules/unit_test/modules/sensordrivers/mts310/test_photo/test_photo.c
@@ -5,28 +5,12 @@
 #include <sys_module.h>
 #include <mts310sb.h>
 
-/*
- * TODO : Define Message here
- */
-enum {
-	MSG_APPMSG1      =    ( MOD_MSG_START + 0 ),
-	MSG_APPMSG2      =    ( MOD_MSG_START + 1 ),
-	// WARNING: number of message cannot be more than 32
-	MSG_LAST_MSG     =    ( MOD_MSG_START + 31 ),  
-};
-
-typedef struct _sos_state_t {
-	// TODO : Add subscribed function here, first example is given
-	// func_cb_ptr func1;
-	// TODO : Add application specific state
-} _sos_state_t;
-
 static int8_t _sos_handler(void *state, Message *msg);
 
-static mod_header_t mod_header SOS_MODULE_HEADER =
+static const mod_header_t mod_header SOS_MODULE_HEADER =
 {
 	.mod_id          = DFLT_APP_ID0,
-	.state_size      = sizeof ( _sos_state_t ),
+	.state_size      = 0,
 	.num_timers      = 0,
 	.num_sub_func    = 0,
 	.num_prov_func   = 0,
@@ -34,18 +18,11 @@ static mod_header_t mod_header SOS_MODULE_HEADER =
 	.processor_type = MCU_TYPE,
 	.code_id         = ehtons(DFLT_APP_ID0),
 	.module_handler  = _sos_handler,
-	/* TODO : uncomment to use function pointer pointer 
-	 * TODO : update num_sub_func as needed
-	.funct = {
-		[0] = {error_16, "Svv0", <Module ID>, <Function ID>},
-	}
-	*/
 };
 
 
 static int8_t _sos_handler ( void *state, Message *msg )
 {
-	//_sos_state_t *s = ( _sos_state_t * ) state;
 	switch ( msg->type ) {
 		case MSG_TIMER_TIMEOUT: {
 			sys_sensor_get_data(MTS310_PHOTO_SID);
@@ -62,7 +39,6 @@ static int8_t _sos_handler ( void *state, Message *msg )
 			return SOS_OK;
 		}
 		case MSG_INIT: {
-			// TODO : uncomment to use the timer 0 of 1 second (1024 counts)
 			sys_timer_start ( 0, 64L, TIMER_REPEAT );
 			return SOS_OK;
 		}
