1eba932a4f77e721ff1a3a70e3f30158c892fdb9
simonhan <simonhan@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-06-09 00:09:38 +0000

    change script

--- tools/avrora/scripts/profileblank.sh
+++ tools/avrora/scripts/profileblank.sh
@@ -13,15 +13,14 @@ java -Xms512m -Xmx512m avrora/Main \
 	-sections=.data,.text,.sos_bls \
 	-random-seed=151079 \
 	-update-node-id=true \
-	-monitors=sos.monitor.SOSPacketMonitor,serial,real-time,sos.monitor.HeapPrintMonitor,sos.monitor.CallTimeMonitor \
+	-monitors=sleep,sos.monitor.SOSPacketMonitor,serial,real-time,sos.monitor.HeapPrintMonitor,sos.monitor.CallTimeMonitor \
 	-ports=0:0:8314 \
 	-topology="./grid5x5.top"   \
 	-sensor-data="light":0:light.dat \
-	-nodecount=2 \
+	-nodecount=25 \
 	-update-node-id=true    \
 	-colors=false \
-	-seconds=10.0 \
-	-base-addr=415 \
+	-seconds=500.0 \
 	-methods=sos_blk_mem_alloc,sos_blk_mem_free,ker_codemem_read,ker_fntable_subscribe,sos_blk_mem_change_own,sos_blk_mem_realloc,post_long,ker_msg_take_data,ker_sys_post_value_do,post_link,ker_sensor_get_data,ker_shm_open,ker_shm_update,ker_shm_close,ker_shm_get,ker_shm_wait,ker_shm_stopwait,ker_timer_start,ker_timer_restart,ker_timer_stop,malloc_gc_kernel,malloc_gc_module \
 ${SOSROOT}/config/test_all/module_test_app.od
 
