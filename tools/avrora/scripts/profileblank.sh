7a1835df45c6057836526fac85eae9ca69f98a5c
simonhan <simonhan@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-06-13 00:06:42 +0000

    update profiling script

--- tools/avrora/scripts/profileblank.sh
+++ tools/avrora/scripts/profileblank.sh
@@ -21,6 +21,6 @@ java -Xms512m -Xmx512m avrora/Main \
 	-update-node-id=true    \
 	-colors=false \
 	-seconds=500.0 \
-	-methods=sos_blk_mem_alloc,sos_blk_mem_free,ker_codemem_read,ker_fntable_subscribe,sos_blk_mem_change_own,sos_blk_mem_realloc,post_long,ker_msg_take_data,ker_sys_post_value_do,post_link,ker_sensor_get_data,ker_shm_open,ker_shm_update,ker_shm_close,ker_shm_get,ker_shm_wait,ker_shm_stopwait,ker_timer_start,ker_timer_restart,ker_timer_stop,malloc_gc_kernel,malloc_gc_module \
+	-methods=ker_slab_alloc,ker_slab_free,ker_gc_mark,sos_blk_mem_alloc,sos_blk_mem_free,ker_codemem_read,ker_fntable_subscribe,sos_blk_mem_change_own,sos_blk_mem_realloc,post_long,ker_msg_take_data,post_link,ker_sensor_get_data,ker_shm_open,ker_shm_update,ker_shm_close,ker_shm_get,ker_shm_wait,ker_shm_stopwait,ker_timer_start,ker_timer_restart,ker_timer_stop,malloc_gc_kernel,malloc_gc_module,malloc_gc,post_short,0x8c,0x90,0x94,0x98,0x9c,0xa0,0xa4,0xa8,0xac,0xb0,0xb4,0xb8,0xbc,0xc0,0xc4,0xc8,0xcc,0xd0,0xd4,0xd8,0xdc,0xe0,0xe4,0xe8,0xec,0xf0,0xf4,0xf8,0xfc,0x100,0x104 \
 ${SOSROOT}/config/test_all/module_test_app.od
 
