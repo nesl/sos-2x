f285918fafc8820eba1208db5cf6c13d3a3bbeeb
pmawhorter <pmawhorter@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-06-20 18:59:16 +0000

    Initial commit of sos_catch. This code complied on a PowerPC Mac Mini at this position in the tree. Of course, that compile hasn't been tested, although other complies have been successful on both a Mac Mini PowerPC and Mac Mini Intel. README and comments in the code describe usage. Because it includes copied code from test_sossrv_client.c, I've included a copy of the GPL that was present in tools/sos_server/. Send any comments/problems to pmawhorter@hmc.edu.
    -Peter Mawhorter

---
+++ tools/sos_catch/catch_surge.c
@@ -0,0 +1,86 @@
+/**
+ * @file catch_surge.c
+ * @breif handles tree routing messages enclosing surge messages
+ * @author Peter Mawhorter (pmawhorter@cs.hmc.edu)
+ */
+
+#include <stdio.h>
+
+/*
+ * The relevant message structures are defined here:
+ */
+#include <message_types.h>
+#include <tree_routing.h>
+#include <surge.h>
+
+/*
+ * This function is set up to recieve sos messages by sos_dump.c
+ * Write whatever custom message handler you want here.
+ */
+int catch(Message* msg) {
+  int i; // loop variable
+
+  if (msg->type != MSG_TR_DATA_PKT) {
+    printf("--------------------------------\n");
+    printf("Dest Mod Id: %d\n", msg->did);
+    printf("Src  Mod Id: %d\n", msg->sid);
+    printf("Dest Addr  : %X\n", msg->daddr);
+    printf("Src  Addr  : %X\n", msg->saddr);
+    printf("Msg Type   : %d\n", msg->type);
+    printf("Msg Length : %d\n", msg->len);
+  } else {
+    // first, parse the data into the tree routing header and surge message:
+    char tree_routing_header[sizeof(tr_hdr_t)];
+    char surge_message[sizeof(tr_hdr_t)];
+    for(i=0; i < sizeof(tr_hdr_t); ++i) {
+      tree_routing_header[i] = msg->data[i];
+    }
+    for(i=0; i < sizeof(SurgeMsg); ++i) {
+      surge_message[i] = msg->data[sizeof(tr_hdr_t)+i];
+    }
+
+    // cast the pieces to pointers:
+    tr_hdr_t* tr_hdr = (tr_hdr_t*)(tree_routing_header);
+    SurgeMsg* sg_msg = (SurgeMsg*)(surge_message);
+
+    // flip the endiannesses of the relevant fields:
+    tr_hdr->originaddr = entohs(tr_hdr->originaddr);
+    tr_hdr->seqno = entohs(tr_hdr->seqno);
+    tr_hdr->parentaddr = entohs(tr_hdr->parentaddr);
+
+    // print the tree routing header:
+    printf("Origin address: %d\n", tr_hdr->originaddr);
+    printf("TR Sequence number: %d\n", tr_hdr->seqno);
+    printf("Hop count: %d\n", tr_hdr->hopcount);
+    printf("Origin hop count: %d\n", tr_hdr->originhopcount);
+    printf("Destination Module: %X (==%d)\n", tr_hdr->dst_pid, tr_hdr->dst_pid);
+    printf("Parent address: %d\n", tr_hdr->parentaddr);
+
+    // print the surge message:
+    printf("------------------\n");
+    printf("Surge message type: %d\n", sg_msg->type);
+    if (sg_msg->type == 7) { //LAME HACK = ILLITERATE
+      printf("Mote working, MDA dysfunctional\n\n");
+      return 0;
+    }
+
+    // print the reading:
+    if (tr_hdr->originaddr != 1) {
+      sg_msg->reading = ntohs(sg_msg->reading);
+      printf("Reading: %d\n", sg_msg->reading);
+    }
+    
+    printf("Origin address: %d\n", sg_msg->originaddr);
+    sg_msg->seq_no = entohl(sg_msg->seq_no);
+    printf("PS Sequence number: %d\n", sg_msg->seq_no);
+    
+    printf("Message Data: \n");
+    for (i = 0; i < msg->len; ++i) {
+      if ((i%16) == 0) printf("\n");
+      printf("%X ", msg->data[i]);
+    }
+  }
+
+  printf("   CRC: %X %X\n", msg->data[msg->len], msg->data[msg->len+1]);
+  printf("\n");
+}
---
+++ tools/sos_catch/sos_catch.c
@@ -0,0 +1,108 @@
+/**
+ * @file sos_catch.c
+ * @breif SOS message subscriber
+ * @author Peter Mawhorter (pmawhorter@cs.hmc.edu)
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sossrv_client.h>
+
+/**
+ * Connects to sossrv.exe and subscribes the catch function to all messages.
+ * * To customize behavior, write your own C code that implements a catch
+ * function in line with the recv_msg_func_t specs and include that
+ * file instead of catch_surge.c (see below).
+ */
+
+/*
+ * Implements the function used to catch messages. Redefine that function
+ * in order to change the behavior of sos_catch:
+ */
+
+#include "catch_surge.c"
+
+/*
+ * Don't ask me why: this is from test_sossrv_client.c
+ * It's used for posting a message to sos_serve that initiates message
+ * handling.
+ */
+#define MSG_PC_TO_MOTE  32
+
+extern char* optarg;
+extern int connected;
+
+int main(int argc, char *argv[])
+{
+  char* ADDRESS = "127.0.0.1"; // Default is localhost
+  char* PORT = "7915";         // This is the default port
+  char c;
+
+  /*
+   * Parse the options using getopt:
+   */
+  while(-1 != (c = getopt(argc, argv, "c:hr:"))) {
+    switch (c) {
+      case 'c': PORT=optarg; break;
+      case 'h': { printf("Usage: sos_dump [-h] [-c PORT] [-r HOST]\n");
+                  printf("Flag summary:\n");
+                  printf("  -c [PORT]      Connect  Connect to sossrv on port PORT. Default is 7915.\n");
+                  printf("  -h           Help       Display this message and quit.");
+                  printf("  -r [ADDRESS]   Remote   Connect to sossrv at ADDRESS (a dotted-decimal IP\n");
+                  printf("                          address). Default is localhost.\n");
+                exit(EXIT_SUCCESS);
+              } break; // technically unnecessary
+      case 'r': ADDRESS=optarg; break;
+    }
+  }
+
+  /*
+   * Connect to sossrv and give an error if we can't:
+   */
+  if (sossrv_connect(ADDRESS, PORT) < 0 || connected != 1) {
+    printf("Could not connect to sossrv at %s on port %s.", ADDRESS, PORT);
+    exit(EXIT_FAILURE);
+  }
+
+  /*
+   * Set up the catch() function (from catch.c) to receive messages.
+   * sossrv_recv_msg closes the socket after starting a new thread, so we
+   * don't have to worry about cleanup.
+   */
+  if (sossrv_recv_msg((recv_msg_func_t)&catch) < 0) {
+    printf("Could not subscribe to sossrv messages.");
+    exit(EXIT_FAILURE);
+  }
+
+  /*
+   * Call the sos_msg_dipatcher function to start getting messages.
+   */
+  sos_msg_dispatcher();
+
+  return 0;
+}
+
+//------------------------------------------------------------------
+// SOS Message Dispatcher
+//------------------------------------------------------------------
+// Scavenged from test_sossrv_client.c
+// This just activates message reception and then goes into an infinite loop.
+int sos_msg_dispatcher()
+{
+    sossrv_post_msg(
+			DFLT_APP_ID0, 
+			DFLT_APP_ID0, 
+			MSG_PC_TO_MOTE, 
+			0, 
+			NULL, 
+			65534, 
+			65535); 
+  while (1){
+
+  }
+  return 0;
+}
