121e1105f89e6110d4432953243aaa0db2d1ca11
keiths <keiths@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-07-02 19:52:08 +0000

    accel test for the mts310 sensor driver

---
+++ modules/unit_test/modules/sensordrivers/mts310/accel/run_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#echo "install a blank kernel"
+cd $SOSROOT/config/blank
+make clean
+make micaz
+make install PROG=mib510 PORT=/dev/ttyUSB0 ADDRESS=0 SOS_GROUP=101
+
+echo "install the sensor driver"
+cd $SOSROOT/modules/sensordrivers/mts310/accel
+make clean
+make mica2 > /dev/null
+sos_tool.exe --insmod=accel_sensor.mlf
+
+sleep 5
+
+echo "install the test driver"
+cd $SOSROOT/modules/unit_test/modules/sensordrivers/mts310/accel
+make clean
+make mica2 > /dev/null
+sos_tool.exe --insmod=accel_test.mlf
