c0dc2d93ec7849cab8f1e721b860573e96d141db
rahulb <rahulb@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-08-22 00:05:17 +0000

    Added loader to DVM test config

--- config/VM/VM_test/vm_config.c
+++ config/VM/VM_test/vm_config.c
@@ -5,9 +5,11 @@
 
 mod_header_ptr dvm_get_header();
 //mod_header_ptr script_loader_get_header();
+mod_header_ptr loader_get_header();
 
 void sos_start(void)
 {
+	ker_register_module(loader_get_header());
   ker_register_module(dvm_get_header());
   //ker_register_module(script_loader_get_header());
 }
