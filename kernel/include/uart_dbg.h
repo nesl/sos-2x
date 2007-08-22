ed2ca308b015f17c8f5bdb2f31cb9d6fb8bf415a
schmid <schmid@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-08-22 16:04:22 +0000

    - objects don't get removed anymore at the end of a make sim - updated UART_DEBUG to sys api

--- kernel/include/uart_dbg.h
+++ kernel/include/uart_dbg.h
@@ -10,28 +10,26 @@
 #ifndef _UART_DBG_H
 #define _UART_DBG_H
 
-#include <sos_info.h>
-#include <malloc.h>
-#include <message.h>
+#include <sys_module.h>
 
 /* id must be a unique char string that will be appended to var instantation
  * to prevent namespace conflicts name */
 #ifdef UART_DEBUG
-#define UART_DBG_MSG_LEN 4
-#define UART_DBG(name, flg0, flg1, flg2, flg3, pid) { \
-	uint8_t *uart_msg##name = ker_malloc(UART_DBG_MSG_LEN, pid); \
-	if(uart_msg##name) { \
-		uart_msg##name[0] = (flg0); \
-		uart_msg##name[1] = (flg1); \
-		uart_msg##name[2] = (flg2); \
-		uart_msg##name[3] = (flg3); \
-		post_uart(pid, pid, 69, UART_DBG_MSG_LEN, uart_msg##name, SOS_MSG_RELEASE, UART_ADDRESS); \
+#define UART_DBG_MSG_LEN (4*sizeof(uint32_t))
+#define UART_DBG(flg0, flg1, flg2, flg3) { \
+	uint32_t *uart_msg = (uint32_t*)sys_malloc(UART_DBG_MSG_LEN); \
+	if(uart_msg) { \
+		uart_msg[0] = (flg0); \
+		uart_msg[1] = (flg1); \
+		uart_msg[2] = (flg2); \
+		uart_msg[3] = (flg3); \
+		sys_post_uart(0, UART_DBG_MSG_LEN, uart_msg, SOS_MSG_RELEASE, BCAST_ADDRESS); \
 	} else { \
-		ker_free(uart_msg##name); \
+		sys_free(uart_msg##name); \
 	} \
 }
 #else
-#define UART_DBG(name, flg0, flg1, flg2, flg3, pid)
+#define UART_DBG(flg0, flg1, flg2, flg3)
 #endif
 
 #endif
