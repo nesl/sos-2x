9aeed4d006f972c9c954c59deb996255748ffd4b
akhi3030 <akhi3030@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-09-07 03:30:29 +0000

    Working on i2c initialization for invent_sensor

---
+++ platform/tmote/invent_sensor_init.c
@@ -0,0 +1,59 @@
+/* -*- Mode: C; tab-width:2 -*- */
+/* ex: set ts=2 shiftwidth=2 softtabstop=2 cindent: */
+/* 
+ * Authors: Akhilesh Singhania
+ *
+ */
+#include <msp430/usart.h>
+#include <sos.h>
+
+#define NO_DEVICES 2
+uint8_t devices[NO_DEVICES] = 
+  { 0x2C, //! Microphone Gain Control
+    0x2D  //! Microphone Compression Control
+  };
+static uint8_t counter = 0;
+
+void invent_sensor_init( void ) 
+{
+  if (NO_DEVICES < 1) return;
+
+  // I2C Module Initialization
+  // setting USART to I2C
+  U0CTL &= ~(1<<I2CEN);
+  U0CTL |= ((1<<SYNC) | (1<<I2C));
+  // Setting I2C data register
+  // Setting to transmit mode and data length to word
+  I2CTCTL &= ~(1<<I2CWORD);
+  I2CTCTL |=  (1<<I2CTRX);
+  // Set to Master Mode and in idle
+  I2CTCTL &= ~((1<<I2CRM) | (1<<I2CSTP) | (1<<I2CSTT));
+  // Enable interrupts on ARDYIE
+  I2CIE |= (1<<ARDYIE);
+
+  // Sending to the first slave device 
+  I2CDRB = devices[counter++] | 0x80;
+  I2CNDAT = 1;
+
+  // Start the transmission
+  I2CTCTL &= ~(1<<I2CRM);
+  I2CTCTL |= ((1<<I2CSTP) | (1<<I2CSTT));
+}
+
+interrupt(USART0RX_VECTOR) i2c_interrupt() 
+{
+  if ((I2CIV & (1<<ARDYIFG)) == 1) {
+    if(counter < NO_DEVICES) {
+      I2CDRB = devices[counter++] | 0x80;
+      I2CNDAT = 1;
+      // Start the transmission
+      I2CTCTL &= ~(1<<I2CRM);
+      I2CTCTL |= ((1<<I2CSTP) | (1<<I2CSTT));
+    }
+    else {
+      // Disable and Reset I2C
+      U0CTL &= ~((1<<I2C) | (1<<SYNC) | (1<<I2CEN));
+      U0CTL |= (1<<SWRST);
+    }
+  }
+}
