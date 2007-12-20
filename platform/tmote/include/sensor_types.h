b1010dc4a867158d48f390486d29d5f0de11d2a0
rahulb <rahulb@41346f4f-9218-0410-8fd7-88ad852d3538>
2007-12-20 03:32:44 +0000

    Updated data types in sensor data structure in new sensing API for tmotes

--- platform/tmote/include/sensor_types.h
+++ platform/tmote/include/sensor_types.h
@@ -1,15 +1,17 @@
 #ifndef _SENSOR_TYPES_H_
 #define _SENSOR_TYPES_H_
 
+typedef uint8_t sensor_id_t;
+
 #ifdef TMOTE_INVENT_SENSOR_BOARD
 // Sensor ID's for Tmote Invent platform and sensor board.
 // Add new sensor ID's here when you add drivers for them.
-typedef enum {
+enum {
 	LIGHT_AMBIENT_SENSOR    = 0,
 	ACCEL_X_SENSOR,
 	ACCEL_Y_SENSOR,
 	MIC_SENSOR,
-} sensor_id_t;
+};
 // Update this number if the total number of sensors
 // increases beyond this current limit.
 #define MAX_NUM_SENSORS   4
@@ -20,12 +22,12 @@ typedef enum {
 // Sensor ID's for Tmote Sky platform and custom ADXL321 sensor
 // board for measuring Impact Shock.
 // Add new sensor ID's here when you add drivers for them.
-typedef enum {
+enum {
 	ACCEL_X_SENSOR	= 0,
 	ACCEL_Y_SENSOR,
 	ACCEL_X_FILTER,
 	ACCEL_Y_FILTER,
-} sensor_id_t;
+};
 // Update this number if the total number of sensors
 // increases beyond this current limit.
 #define MAX_NUM_SENSORS   4
@@ -33,7 +35,7 @@ typedef enum {
 #else
 // Sensor ID's for Tmote Sky platform with optional sensors.
 // Add new sensor ID's here when you add drivers for them.
-typedef enum {
+enum {
 	LIGHT_AMBIENT_SENSOR    = 0,
 	LIGHT_PAR_SENSOR,
 	HUMIDITY_SENSOR,
@@ -41,13 +43,12 @@ typedef enum {
 	INTERNAL_TEMPERATURE_SENSOR,
 	ACCEL_X_SENSOR,
 	ACCEL_Y_SENSOR,
-} sensor_id_t;
+};
 // Update this number if the total number of sensors
 // increases beyond this current limit.
 #define MAX_NUM_SENSORS   8
 
 #endif
-
 #endif
 
 
